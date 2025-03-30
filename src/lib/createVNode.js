6b467dba3def460b4ce458744bc426b6d30641ab
unseoJang <qqbck123@naver.com>
2025-03-30 20:59:55 +0900

    createVNod 수정

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,11 @@
+// 객체 반환
 export function createVNode(type, props, ...children) {
-  return {};
+  // console.log("type", type);
+  // console.log("props", props);
+  // console.log("children", children);
+  return {
+    type,
+    props,
+    children,
+  };
 }
