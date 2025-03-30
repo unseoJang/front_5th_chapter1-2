56337d07cf22f33e8139360d91d7b94178e56425
unseoJang <qqbck123@naver.com>
2025-03-30 21:08:34 +0900

    가상 돔 평탄화

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -6,6 +6,6 @@ export function createVNode(type, props, ...children) {
   return {
     type,
     props,
-    children,
+    children: children.flat(), // React나 가상 DOM 기반으로 뭔가를 렌더링할 때 children을 배열로 받아서 사용하는 경우가 많은데, 실수로 중첩된 배열이 들어가는 경우가 있어요.
   };
 }
